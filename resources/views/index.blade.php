f69d7e65dbeb1a8072911f393c326b8610d071a6
HADO564 <70658014+HADO564@users.noreply.github.com>
2022-05-31 21:27:00 +0500

    added a landing page. although its terrible, its the best i could do

---
+++ resources/views/index.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Landing Page</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Responsive navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#!">Lettuce Talk</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Header - set the background image for the header in the line below-->
+    <header class="pt-5 pb-3 bg-image-full" style="background-image: url('https://media.istockphoto.com/photos/galaxy-outer-space-starry-sky-purple-red-abstract-star-pattern-picture-id1347283792?b=1&k=20&m=1347283792&s=170667a&w=0&h=xCJGDFW4tX_REGXIKIaMTaRqAl4TPoKxFSXCGGGBJyo=')">
+        <div class="text-center mt-5 mb-3 ">
+            <h1 class="text-white fs-3 fw-bolder">Hello Adventurer!</h1>
+            <p class="text-white-50 mb-0">And welcome to our humble landing page</p>
+        </div>
+    </header>
+    <!-- Content section-->
+    <section class="py-5 " style="background-position:center;background-size:cover;background-repeat:no-repeat;background-image: url('https://cdn.pixabay.com/photo/2020/05/18/16/17/social-media-5187243_960_720.png')">
+        <div class="container my-5">
+            <div class="justify-content-center">
+                <div class="col-lg-3">
+                    <h1 class="fs-3 fw-bolder py-2 mg-1" style="border-radius:5px ;background-color:rgb(115, 102, 153);color:rgb(255,255,255);text-shadow: 2px,2px;padding: right -20px;">Have you registered yet?</h1><br>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style="background-color:purple;"><a href="/register" style="text-decoration:none; color:white">Register!</a></button>
+                </div>
+                <br>
+                <div class="col-lg-2 py-5">
+                    <h1 class="fs-3 fw-bolder py-2 mg-1" style="border-radius:5px ;background-color:rgb(115, 102, 153);color:rgb(255,255,255);text-shadow: 2px,2px;padding: right -20px;">Already a user?</h1><br>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style="background-color:purple;"><a href="/login" style="text-decoration:none; color:white">Login!</a></button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Image element - set the background image for the header in the line below-->
+    <div class="py-5 bg-image-full" style="background-image: url('https://source.unsplash.com/4ulffa6qoKA/1200x800')">
+        <!-- Put anything you want here! The spacer below with inline CSS is just for demo purposes!-->
+        <div class="py-3 text-white fs-4 justify-center" style="height: 20rem;text-align:center">
+            This project is a work of Haider, Ali and Moosah.
+        </div>
+    </div>
+    <!-- Content section-->
+    <section class="py-5">
+        <div class="container my-5">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <h2>Engaging Background Images</h2>
+                    <p class="lead">The background images used in this template are sourced from Unsplash and are open source and free to use.</p>
+                    <p class="mb-0">I can't tell you how many people say they were turned off from science because of a science teacher that completely sucked out all the inspiration and enthusiasm they had for the course.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
